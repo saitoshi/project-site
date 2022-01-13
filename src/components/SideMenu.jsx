2d91bf8f0809a6b4b2593249e3dfafffef093cc6
Shun <47809066+saitoshi@users.noreply.github.com>
2022-01-13 13:17:37 +0900

    Update SideMenu.jsx

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,28 +1,56 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import { Menu, Sticky, Sidebar, Icon } from 'semantic-ui-react';
+import { Menu, Button, Sidebar, Icon } from 'semantic-ui-react';
 import './style.css';
 
-function SideMenu() {
+class SideMenu extends React.Component {
+  state = {
+    visible: false,
+    visible2: false,
+  }
+
+  handleShowClick = () => {
+    this.setState((prevState) => ({
+      visible: !prevState.visible,
+    }));
+  }
+
+  handleShowClick2 = () => {
+    this.setState((prevState) => ({
+      visible2: !prevState.visible2,
+    }));
+  }
+  render() {
+    const { visible } = this.state;
+  
   return (
-      <Sticky>
-      <Sidebar
-        as = {Menu}
-        animation='overlay'
-        icon='labeled'
-        inverted
-        color= 'blue'
-        vertical
-        visible
-        width='thin'>
-        <Menu.Item header as={NavLink} exact to={"/"}><Icon name='user circle outline'/>SHIN'S WORK </Menu.Item>
+    <div>
+    <Button style={{ position: 'fixed', zIndex: 1, top: 0, height: '100vh', borderRadius: 0 }} attached={'right'} icon color='teal' disabled={false} onClick={this.handleShowClick}>
+     <Icon name='bars'/>
+   </Button>
+   <Sidebar 
+   as = {Menu}
+   animation='push'
+   direction='left'
+   icon = 'labeled'
+   vertical 
+   inverted
+   visible = {visible}
+   width='thin'
+   color = 'teal'
+   style={{ height: '100vh', minHeight: '100vh' }}>
+           <Button fluid style={{ zIndex: 2 }} icon color='teal' disabled={false} onClick={this.handleShowClick}>
+       <Icon name='bars'/>
+       <br/>
+       <Menu.Item header as={NavLink} exact to={"/"}><Icon name='user circle outline'/>SHIN'S WORK </Menu.Item>
         <Menu.Item as={NavLink} exact to ={"/resume"}><Icon name='star outline'/> ABOUT</Menu.Item>
         <Menu.Item as={NavLink} exact to ={"/project"}><Icon name ='edit outline'/> WORK</Menu.Item>
         <Menu.Item as={NavLink} exact to ={"/blog"}><Icon name ='sticky note outline'/> BLOG</Menu.Item>
         <Menu.Item as={NavLink} exact to ={"/about"}><Icon name ='flag outline'/> 日本語</Menu.Item>
-      </Sidebar>
-      </Sticky>
+     </Button>
+   </Sidebar>
+ </div>
 
-  );
+  );}
 }
 export default SideMenu;
