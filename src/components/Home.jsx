f1139d21000873bec1744840950c5fb8e87afe7e
Shun <47809066+saitoshi@users.noreply.github.com>
2022-02-26 15:43:16 +0900

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,9 +19,6 @@ function Home() {
       <Grid>
         <Grid.Column textAlign="center">
           <Button.Group centered>
-            <Button color='twitter' size='medium' href="https://twitter.com/iseyadj45">
-                <Icon name='twitter'/> <br/> Follow me on Twitter
-            </Button>
             <Button color='red' size='medium' href="mailto:saitoshi2145@gmail.com">
               <Icon name='envelope'/> <br/> Contact me through email
             </Button>
